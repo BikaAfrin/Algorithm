e4def6b8527fa10b18b52e6a3344203853bd2b5d
BikaAfrin <84857753+BikaAfrin@users.noreply.github.com>
2021-12-01 11:35:46 +0600

    Create BFS.cpp

---
+++ Graph Theory/BFS.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#define ll long long
+#define N 8
+using namespace std;
+
+vector<int>adj[N];
+int vis[N];
+
+void BFS(int node)
+{
+    queue<int>q;
+    q.push(node);
+    vis[node]=1;
+    while(!q.empty())
+    {
+        int a=q.front();
+        cout<<a<<" ";
+        q.pop();
+
+        for(auto u:adj[a])
+        {
+            if(!vis[u])
+            {
+                q.push(u);
+                vis[u]=1;
+            }
+        }
+    }
+}
+int main()
+{
+    int t;cin>>t;
+
+    for(int i=0;i<t;i++)
+    {
+        int a,b;cin>>a>>b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+    for(int i=0;i<N;i++)
+    {
+        if(!vis[i])
+        {
+            BFS(i);
+        }
+    }
+}
